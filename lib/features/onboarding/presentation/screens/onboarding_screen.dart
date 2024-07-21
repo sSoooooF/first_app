270f69afaf2de0cb79bf650dcdd807daf9946f47
DmitrDomrachev <domrachev@surf.dev>
2024-07-21 23:00:30 +0500

    feat: page index indicator

--- lib/features/onboarding/presentation/screens/onboarding_screen.dart
+++ lib/features/onboarding/presentation/screens/onboarding_screen.dart
@@ -3,23 +3,25 @@ import 'package:first_app/features/onboarding/presentation/widgets/onboarding_wi
 import 'package:flutter/material.dart';
 
 class OnboardingScreen extends StatefulWidget {
-  static const _onboardingData = [
-    OnboardingEntity(
-      title: 'Welcome to the App 1',
-      description: 'This is a simple onboarding screen 1',
-      image: 'images/icon.svg',
-    ),
-    OnboardingEntity(
-      title: 'Welcome to the App 2',
-      description: 'This is a simple onboarding screen 2',
-      image: 'images/icon.svg',
-    ),
-    OnboardingEntity(
-      title: 'Welcome to the App 3',
-      description: 'This is a simple onboarding screen 3',
-      image: 'images/icon.svg',
-    ),
-  ];
+  static const _onboarding = OnboardingEntity(
+    pages: [
+      OnboardingPageEntity(
+        title: 'Welcome to the App 1',
+        description: 'This is a simple onboarding screen 1',
+        image: 'images/icon.svg',
+      ),
+      OnboardingPageEntity(
+        title: 'Welcome to the App 2',
+        description: 'This is a simple onboarding screen 2',
+        image: 'images/icon.svg',
+      ),
+      OnboardingPageEntity(
+        title: 'Welcome to the App 3',
+        description: 'This is a simple onboarding screen 3',
+        image: 'images/icon.svg',
+      ),
+    ],
+  );
 
   const OnboardingScreen({
     super.key,
@@ -30,28 +32,18 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
-  late OnboardingEntity currentEntity;
-
-  @override
-  void initState() {
-    super.initState();
-    currentEntity = OnboardingScreen._onboardingData.first;
-  }
+  int index = 0;
 
   void _onNextPressed() {
-    final currentIndex =
-        OnboardingScreen._onboardingData.indexOf(currentEntity);
-
-    if (currentIndex < OnboardingScreen._onboardingData.length - 1) {
-      setState(() {
-        currentEntity = OnboardingScreen._onboardingData[currentIndex + 1];
-      });
-    }
+    if (index > OnboardingScreen._onboarding.pagesCount - 1) return;
+    setState(() {
+      index++;
+    });
   }
 
   void _onSkipPressed() {
     setState(() {
-      currentEntity = OnboardingScreen._onboardingData.last;
+      index = OnboardingScreen._onboarding.pagesCount - 1;
     });
   }
 
@@ -66,11 +58,11 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               ContentBloc(
-                title: currentEntity.title,
-                body: currentEntity.description,
-                pageIndex: 1,
-                svgImage: currentEntity.image,
-                pageCount: 3,
+                title: OnboardingScreen._onboarding.getPage(index).title,
+                body: OnboardingScreen._onboarding.getPage(index).description,
+                pageIndex: index,
+                svgImage: OnboardingScreen._onboarding.getPage(index).image,
+                pageCount: OnboardingScreen._onboarding.pagesCount,
               ),
               ControlButtons(
                 onNextPressed: _onNextPressed,
