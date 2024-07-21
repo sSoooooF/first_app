7118c94f8751aacede61bf41d37b6e75d267fcad
Nail <nurievnail03@mail.ru>
2024-07-21 20:05:42 +0300

    refactoring code

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/util/extenshionList.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -36,32 +37,45 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const MyHomePage(
-        title: '',
+      home: OnboardingScreen(
+        'Задачник',
+        'Создай и управляй своими делами в UpTodo',
+        'images/icon.svg',
+        () {},
+        () {},
       ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
+class OnboardingScreen extends StatelessWidget {
+  final String _title;
+  final String _subtitle;
+  final String _imagePath;
+  final VoidCallback _onSkipButtonPressed;
+  final VoidCallback _onNextButtonPressed;
 
-  final String title;
+  const OnboardingScreen(this._title, this._subtitle, this._imagePath,
+      this._onSkipButtonPressed, this._onNextButtonPressed);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         body: Padding(
-      padding: EdgeInsets.only(top: 116, left: 32, right: 32, bottom: 32),
+      padding: const EdgeInsets.only(top: 116, left: 32, right: 32, bottom: 32),
       child: Column(
         children: [
           Content(
-            'Управляй своими задачами',
-            'Вы можете легко управлять всеми своими задачами в UpTodo',
-            'icon.svg',
+            title: _title,
+            subtitle: _subtitle,
+            imagePath: _imagePath,
+            curPage: 0,
+            maxPages: 3,
           ),
-          Spacer(),
-          Buttons(),
+          const Spacer(),
+          Buttons(
+              onSkipButtonPressed: _onSkipButtonPressed,
+              onNextButtonPressed: _onNextButtonPressed),
         ],
       ),
     ));
@@ -69,35 +83,26 @@ class MyHomePage extends StatelessWidget {
 }
 
 class Buttons extends StatelessWidget {
-  const Buttons();
+  final VoidCallback onSkipButtonPressed;
+  final VoidCallback onNextButtonPressed;
+
+  const Buttons(
+      {required this.onSkipButtonPressed, required this.onNextButtonPressed});
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       children: [
         TextButton(
-            onPressed: null,
-            child: Text(
+            onPressed: onSkipButtonPressed,
+            child: const Text(
               "Пропустить",
-              style: TextStyle(
-                fontFamily: 'Roboto',
-                color: Colors.white,
-                fontSize: 14,
-                fontWeight: FontWeight.w500,
-              ),
             )),
-        Spacer(),
+        const Spacer(),
         FilledButton(
-          style: ButtonStyle(),
-          onPressed: null,
-          child: Text(
+          onPressed: onNextButtonPressed,
+          child: const Text(
             "Продолжить",
-            style: TextStyle(
-              fontFamily: 'Roboto',
-              color: Colors.white,
-              fontSize: 14,
-              fontWeight: FontWeight.w500,
-            ),
           ),
         )
       ],
@@ -109,51 +114,67 @@ class Content extends StatelessWidget {
   final String title;
   final String subtitle;
   final String imagePath;
+  final int curPage;
+  final int maxPages;
 
-  const Content(this.title, this.subtitle, this.imagePath);
+  const Content(
+      {required this.title,
+      required this.subtitle,
+      required this.imagePath,
+      required this.curPage,
+      required this.maxPages});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          SvgPicture.asset('icon.svg'),
-          const SizedBox(
-            height: 32,
-          ),
-          const SizedBox(
-            height: 32,
-          ),
-          Container(
-            height: 4,
-            width: 100,
-            child: Row(
-              children: List.generate(3, (index) {
-                return Container(
-                    width: 25,
+    final theme = Theme.of(context);
+    final colorScheme = theme.colorScheme;
+    return Column(
+      children: [
+        SvgPicture.asset(
+          imagePath,
+        ),
+        const SizedBox(
+          height: 32,
+        ),
+        const SizedBox(
+          height: 32,
+        ),
+        SizedBox(
+          height: 4,
+          width: 100,
+          child: Row(
+            children: List<Widget>.generate(maxPages, (index) {
+              return Expanded(
+                flex: 4,
+                child: Container(
                     height: 4,
                     decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
+                      color: index == curPage
+                          ? colorScheme
+                              .primary //material-theme/sys/light/secondary-container
+                          : colorScheme.secondaryContainer,
                       borderRadius: BorderRadius.circular(10),
-                    ));
-              }),
-            ),
+                    )),
+              );
+            }).insertBetween(const Spacer(
+              flex: 1,
+            )),
           ),
-          Text(
-            textAlign: TextAlign.center,
-            title,
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          Text(
-            textAlign: TextAlign.center,
-            subtitle,
-            style: Theme.of(context).textTheme.bodyLarge,
-          )
-        ],
-      ),
+        ),
+        Text(
+          textAlign: TextAlign.center,
+          title,
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Text(
+          textAlign: TextAlign.center,
+          subtitle,
+          style: Theme.of(context).textTheme.bodyLarge,
+        )
+      ],
     );
   }
 }
